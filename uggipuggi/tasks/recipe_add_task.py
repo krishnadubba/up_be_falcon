23e15900a4185828ae7ee12a57084f1f25027cce
kdubba <krishna.dubba@nokia.com>
2017-10-16 17:28:56 +0100

    adding celery logging

--- uggipuggi/tasks/recipe_add_task.py
+++ uggipuggi/tasks/recipe_add_task.py
@@ -1,8 +1,12 @@
 import os, sys
 from bson import json_util
+from celery.utils.log import get_task_logger
 from uggipuggi.celery.celery import celery
+
+logger = get_task_logger(__name__)
 
 @celery.task
 def user_feed_add_recipe(message):
     user_id, activity_id, status = json_util.loads(message.strip("'<>() ").replace('\'', '\"'))
-    print('################# I am executed in celery worker ###################')
+    logger.debug('################# I am executed in celery worker ###################')
+    
