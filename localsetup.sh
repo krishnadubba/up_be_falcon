8149214d3173a4fca7f8d8a153791cd0b960330f
krishna <krishna.dubba@gmail.com>
2017-11-05 19:30:48 +0000

    helpful commands for localsetup

--- localsetup.sh
+++ localsetup.sh
@@ -2,3 +2,8 @@
 docker swarm init
 docker build -t kr .
 docker stack deploy -c localsetup.yml uggi
+xdg-open http://172.17.0.1/test
+xdg-open http://172.17.0.1/visualizer
+echo '======================================================'
+echo 'For logs: docker service logs -f stackname_servicename'
+echo '======================================================'
