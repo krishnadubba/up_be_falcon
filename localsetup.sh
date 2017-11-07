c447df16570fc2075300185f94b33d162178efd0
kdubba <krishna.dubba@nokia.com>
2017-11-07 16:17:14 +0000

    Add creating proxy overlay network in the docker swarm start file

--- localsetup.sh
+++ localsetup.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 docker swarm init
+docker network create -d overlay proxy
 docker build -t kr .
 docker stack deploy -c localsetup.yml uggi
-xdg-open http://172.17.0.1/test
-xdg-open http://172.17.0.1/visualizer
+DOCKER_GWBRIDGE_IP=`ifconfig docker_gwbridge | awk '/inet addr/ {gsub("addr:", "", $2); print $2}'`
+xdg-open http://$DOCKER_GWBRIDGE_IP/test
+xdg-open http://$DOCKER_GWBRIDGE_IP/visualizer
 echo '======================================================'
 echo 'For logs: docker service logs -f stackname_servicename'
+echo 'Docker Overlay Network:'
+docker network inspect docker_gwbridge --format '{{range $k, $v := index .IPAM.Config 0}}{{.| printf "%s: %s " $k}}{{end}}'
 echo '======================================================'
