96b3599085ec169d7ad80be3482935328bd1874d
krishna <krishna.dubba@gmail.com>
2017-12-05 04:59:43 +0530

    adding service to get group recipes

---
+++ uggipuggi/controllers/group_recipes.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+import falcon
+import logging
+
+from uggipuggi.constants import RECIPE, GROUP_RECIPES, RECIPE_VERY_CONCISE_VIEW_FIELDS
+from uggipuggi.controllers.hooks import serialize, supply_redis_conn
+from uggipuggi.libs.error import HTTPBadRequest, HTTPUnauthorized
+from uggipuggi.messaging.group_kafka_producers import group_recipes_kafka_item_get_producer
+
+
+logger = logging.getLogger(__name__)
+
+@falcon.before(supply_redis_conn)
+class Item(object):
+    def __init__(self):
+        self.kafka_topic_name = 'group_recipes_item'
+
+    # TODO: handle PUT requests
+    @falcon.before(supply_redis_conn)
+    @falcon.after(serialize)
+    @falcon.after(group_recipes_kafka_item_get_producer)
+    def on_get(self, req, resp, id):
+        req.kafka_topic_name = '_'.join([self.kafka_topic_name, req.method.lower()])
+        recipe_ids = req.redis_conn.zrange(GROUP_RECIPES+id, 0, -1)
+        pipeline = req.redis_conn.pipeline(True)
+        for recipe_id in recipe_ids:
+            pipeline.hmget(RECIPE+recipe_id, *RECIPE_VERY_CONCISE_VIEW_FIELDS)
+        all_concise_recipes = [{k: v} for k, v in zip(recipe_ids, pipeline.execute())]
+        resp.body = {'items': all_concise_recipes, 
+                     'fields': RECIPE_VERY_CONCISE_VIEW_FIELDS, 
+                     'count': len(all_concise_recipes)
+                    }
+        resp.status = falcon.HTTP_OK
+        
